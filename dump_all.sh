47673dec6f4c0c639251aff7d536e94ef3a6e4cf
Michaël Schrijver <michael.schrijver@northwave.nl>
2024-03-20 16:28:14 +0100

    Improve dump_all.sh portability. Closes #2

--- dump_all.sh
+++ dump_all.sh
@@ -3,7 +3,7 @@ if [ -z "$1" ] ; then
     echo "usage: $0 image-file"
     exit -1
 fi
-mmls -a "$1" |egrep '^\d+:' | while read line
+mmls -a "$1" |egrep '^[0-9]+:' | while read line
 do
     slot=$(echo $line|awk '{print $2}')
     start=$(echo $line|awk '{print $3}'|sed -e 's/^0*//')
